78c7d82fbac5980efc58656a067211969be34b97
jvfe <jvfecav@gmail.com>
2020-07-07 13:46:41 -0300

    refactor: utils request to new function

--- pytrials/utils.py
+++ pytrials/utils.py
@@ -3,32 +3,30 @@ import csv
 import re
 
 
-def json_handler(url):
-
+def request_ct(url):
     try:
-        response = requests.get(url).json()
-    except IOError:
-        print(
+        response = requests.get(url)
+    except:
+        raise IOError(
             "Couldn't retrieve the data, check your search expression or try again later."
         )
     else:
         return response
 
 
+def json_handler(url):
+    return request_ct(url).json()
+
+
 def csv_handler(url):
 
     # https://stackoverflow.com/questions/35371043/use-python-requests-to-download-csv
 
-    try:
-        response = requests.get(url)
-    except IOError:
-        print(
-            "Couldn't retrieve the data, check your search expression or try again later."
-        )
-    else:
-        decoded_content = response.content.decode("utf-8")
-        split_by_blank = re.split('\n\s*\n', decoded_content) # Divides header info
-        cr = csv.reader(split_by_blank[1].splitlines(), delimiter=",")
-        records = list(cr)
+    response = request_ct(url)
+    decoded_content = response.content.decode("utf-8")
+    split_by_blank = re.split("\n\s*\n", decoded_content)  # Divides header info
+    cr = csv.reader(split_by_blank[1].splitlines(), delimiter=",")
+    records = list(cr)
 
-        return records
+    return records
+
