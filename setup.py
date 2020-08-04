fc969503d559759cc61a0e75c77dc4fcf04bdc8a
jvfe <jvfecav@gmail.com>
2020-08-04 16:26:12 -0300

    Clean setup.py
    Make it more readable, add tags, fix incorrect statements.

--- setup.py
+++ setup.py
@@ -4,42 +4,50 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-setup_requirements = ['pytest-runner', ]
+install_requirements = [
+    "requests",
+]
 
-test_requirements = ['pytest>=3', ]
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="João Vitor F. Cavalcante",
-    author_email='jvfe@ufrn.edu.br',
-    python_requires='>=3.5',
+    author_email="jvfe@ufrn.edu.br",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python wrapper around the clinicaltrials.gov API",
-    install_requires=["requests"],
+    install_requires=install_requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pytrials',
-    name='pytrials',
-    packages=find_packages(include=['pytrials', 'pytrials.*']),
+    keywords="pytrials clinical-trials clinical trials tabular text-mining text opendata",
+    name="pytrials",
+    packages=find_packages(include=["pytrials", "pytrials.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/jvfe/pytrials',
-    version='0.1.2',
+    url="https://github.com/jvfe/pytrials",
+    version="0.1.2",
     zip_safe=False,
 )
