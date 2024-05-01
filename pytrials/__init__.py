ae4283eb24ee014252eca7dfb97cc33c2ec20815
jvfe <jvfecav@gmail.com>
2024-05-01 10:20:41 -0300

    refactor: Add new logic for study fields property
    Signed-off-by: jvfe <jvfecav@gmail.com>

--- pytrials/__init__.py
+++ pytrials/__init__.py
@@ -1,5 +1,11 @@
 """Top-level package for pytrials."""
 
+from pathlib import Path
+
 __author__ = """João Vitor F. Cavalcante"""
 __email__ = "jvfe@ufrn.edu.br"
 __version__ = "0.3.0"
+
+HERE = Path(__file__).parent.resolve()
+
+study_fields = Path(HERE, "fields.csv")
