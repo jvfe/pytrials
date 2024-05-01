1060c846493622ca576bdf8a196f3db29ae923ce
jvfe <jvfecav@gmail.com>
2024-05-01 11:40:28 -0300

    docs: Update docs conf
    Signed-off-by: jvfe <jvfecav@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('..'))
 
 import pytrials
 
@@ -32,24 +31,24 @@ import pytrials
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinxcontrib.napoleon"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = "index"
+master_doc = 'index'
 
 # General information about the project.
-project = "pytrials"
-copyright = "2020, João Vitor F. Cavalcante"
-author = "João Vitor F. Cavalcante"
+project = 'Pytrials'
+copyright = "2024, jvfe"
+author = "jvfe"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
@@ -70,10 +69,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -84,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -95,13 +94,13 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "pytrialsdoc"
+htmlhelp_basename = 'pytrialsdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -110,12 +109,15 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
+
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
+
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
+
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -125,13 +127,9 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (
-        master_doc,
-        "pytrials.tex",
-        "pytrials Documentation",
-        "João Vitor F. Cavalcante",
-        "manual",
-    ),
+    (master_doc, 'pytrials.tex',
+     'Pytrials Documentation',
+     'jvfe', 'manual'),
 ]
 
 
@@ -139,7 +137,11 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "pytrials", "pytrials Documentation", [author], 1)]
+man_pages = [
+    (master_doc, 'pytrials',
+     'Pytrials Documentation',
+     [author], 1)
+]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -148,14 +150,14 @@ man_pages = [(master_doc, "pytrials", "pytrials Documentation", [author], 1)]
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (
-        master_doc,
-        "pytrials",
-        "pytrials Documentation",
-        author,
-        "pytrials",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
+    (master_doc, 'pytrials',
+     'Pytrials Documentation',
+     author,
+     'pytrials',
+     'One line description of project.',
+     'Miscellaneous'),
 ]
+
+
+
 
